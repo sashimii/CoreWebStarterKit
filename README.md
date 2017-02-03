c8ed9f2981f2ead9b91c00b92aa384a68b75ec24
sashimii <stailor@metrolatorsmbp3.torstar.net>
2017-02-03 14:54:05 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Core Web Starter Kit
+
+## Features
+
+1. Foundation
+2. Browsersync
+3. Gulp-Sass
+4. Hot Reloading
+
+
+## Usage
+
+Install: `npm install`
+
+Run Dev Server: `gulp`
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var bundle = require('gulp-bundle-assets');
+var bulk = require('bulk-require');
+var sass = require('gulp-sass');
+
+var files = {
+  css: {
+    src: 'src/scss/*.scss',
+    dist: 'dist/css/'
+  },
+  js: {
+    src: [
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/what-input/dist/what-input.js',
+      'node_modules/foundation-sites/dist/js/foundation.min.js',
+      'src/js/*.js'
+    ],
+    dist: 'dist/js/'
+  }
+}
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'scripts'], () => {
+
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch(files.css.src, ['sass']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', () => {
+  return gulp.src(files.css.src)
+    .pipe(sass())
+    .pipe(gulp.dest(files.css.dist))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+  return gulp.src(files.js.src)
+    .pipe(gulp.dest(files.js.dist));
+});
+
+
+gulp.task('default', ['serve']);
